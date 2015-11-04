269a3cee085f239add934cb098bff518c0222668
Qui Ego <acct.qui@gmail.com>
2015-11-04 15:10:40 +0800

    It now works. Also replaced previous list with a set.

--- main.py
+++ main.py
@@ -7,54 +7,61 @@ def rouletteIntro():
     time.sleep(1)
     print("Your goal is to get the highest amount of blank shots without dying!")
     print("So if you get all 7 blanks, you win!")
-    time.sleep(2)
+    time.sleep(1)
     print("So " + name + ". let the games begin..")
-    time.sleep(5)
 
     bulletChamber = random.randint(1,8)
 
     return bulletChamber
 
 def pickChamber(loadedChamber):
-    shot == 0
-    chosenChambers = []
+    dead = 0
+    chosenChambers = set()
 
-    while shot == 0:
+    while dead == 0:
         chamberCount = len(chosenChambers)
+        chambersLeft = 8 - chamberCount
+        chambersLeft = str(chambersLeft)
+        score = str(chamberCount)
+        print("There are " + chambersLeft + " chambers left.")
+
 
         if chamberCount == 7:
             print("Congratulations! Your prize is..")
-            time.sleep(5)
+            time.sleep(3)
             print("BANG!")
-            break
+            time.sleep(2)
+            print("You've survived " + score + " shots.")
+            shot = 1
 
-        if chamberCount > 7:
-            chamber = input("Pick a chamber from 1 - 8.")
+        else:
+            chamber = input("Pick a chamber from 1 - 8.\n")
             chamber = int(chamber)
 
-        if chamber < 1 or chamber > 8:
-            print("Fuck off.")
+            if chamber < 1 or chamber > 8:
+                print("Fuck off.")
 
-        if chamber == loadedChamber:
-            print(...)
-            time.sleep(5)
-            print("BANG!")
-            break
+            elif chamber == loadedChamber:
+                print("...")
+                time.sleep(3)
+                print("BANG!")
+                time.sleep(2)
+                print("You've survived " + score + " shots.")
+                dead = 1
 
-        if chamber != loadedChamber:
-            print(...)
-            time.sleep(5)
-            print("'click'")
-            chosenChambers.append(chamber)
+            elif chamber != loadedChamber:
+                print("...")
+                time.sleep(5)
+                print("'click'")
+                chosenChambers.add(chamber)
 
-    shot = 1
+
 
 play = "y"
 while play == "y" or play == "Y":
-    rouletteIntro()
 
-    loadedChamber = rouletteIntro()
+    chamber = rouletteIntro()
 
-    pickChamber(bulletChamber)
+    pickChamber(chamber)
 
     play = input("Would you like to play again?(y/n)\n")
